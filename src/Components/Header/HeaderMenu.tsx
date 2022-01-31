060fb312bda11c2c7f31ad34470e413a1cc1b6e3
pooriaahmadi <pooriababak444@gmail.com>
2022-01-31 15:29:22 -0500

    cleaning up

--- src/Components/Header/HeaderMenu.tsx
+++ src/Components/Header/HeaderMenu.tsx
@@ -1,12 +1,17 @@
 import HeaderItem from "./HeaderItem";
+import { useState } from "react";
+import { HeaderItemInputs } from "../../types/interfaces";
+
 
 const HeaderMenu = () => {
-    return <div className="menu">
-        <HeaderItem content="Home" link="#" />
-        <HeaderItem content="Login" link="/login" />
-        <HeaderItem content="IDK" link="#" />
-        <HeaderItem content="About us" link="#" />
+  return (
+    <div className="menu">
+      <HeaderItem content="Home" link="/" />
+      <HeaderItem content="Login" link="/login" />
+      <HeaderItem content="Register" link="/register" />
+      <HeaderItem content="About us" link="#" />
     </div>
+  );
 };
 
 export default HeaderMenu;
