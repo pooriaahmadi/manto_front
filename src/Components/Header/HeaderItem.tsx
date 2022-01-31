9d17b127429c2c6ebd7956fb23b6d9d938da6adb
pooriaahmadi <pooriababak444@gmail.com>
2022-01-31 15:29:04 -0500

    got rid of props

--- src/Components/Header/HeaderItem.tsx
+++ src/Components/Header/HeaderItem.tsx
@@ -1,8 +1,8 @@
 import { HeaderItemInputs } from "../../types/interfaces";
 import { Link } from "react-router-dom";
 
-const HeaderItem = (props: HeaderItemInputs) => {
-    return <Link to={props.link} className="item">{props.content}</Link>
+const HeaderItem = ({content, link}: HeaderItemInputs) => {
+    return <Link to={link} className="item">{content}</Link>
 }
 
 export default HeaderItem;
