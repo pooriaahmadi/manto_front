f0465efdd05a20c0c4879c4512aaabbbe262fcc7
pooriaahmadi <pooriababak444@gmail.com>
2022-01-31 15:28:35 -0500

    added register route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Home from './Pages/Home';
 import Login from './Pages/Login';
+import Register from './Pages/Register';
 import Header from './Components/Header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './assets/scss/style.scss';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
