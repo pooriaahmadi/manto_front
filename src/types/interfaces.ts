312dc8e38814221b91f841c32c939dfa7d275919
pooriaahmadi <pooriababak444@gmail.com>
2022-01-31 15:29:11 -0500

    interfaces update

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -7,7 +7,20 @@ export interface InputInputs {
     type: string;
     placeholder: string;
     title: string;
-    maxLength?: number;
+    maxLength: number;
     handleOnChange: any;
     value: any
+}
+
+export interface TextInput {
+    placeholder: string
+    title: string;
+    handleOnChange: any;
+    value: any;
+}
+
+export interface EmailInputs extends TextInput {}
+export interface PasswordInputs extends TextInput {}
+export interface SubmitInputs {
+    value: string;
 }
