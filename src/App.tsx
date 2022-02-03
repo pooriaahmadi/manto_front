cd49a01d4fa68a3537e6fddf9738f07fd37b92a2
pooriaahmadi <pooriababak444@gmail.com>
2022-02-03 15:23:37 -0500

    yeah better not look at it

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,63 @@
-import Home from './Pages/Home';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Header from './Components/Header/Header';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import User from './Classes/User';
-import { useState } from 'react';
-import './assets/scss/style.scss';
-import { stringify } from 'querystring';
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Header from "./Components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from "./Classes/User";
+import { useState, useEffect } from "react";
+import "./assets/scss/style.scss";
 function App() {
-  const [user, setUser] = useState<User>(new User({endpoint: "http://localhost:3001"}));
+  const [update, setUpdate] = useState<boolean>(false);
+  const [user, setUser] = useState<User>(
+    new User({ endpoint: "http://localhost:3001" })
+  );
+  useEffect(() => {
+    const doStuff = async () => {
+      const token = localStorage.getItem("token");
+      if (token === "" || !token) return;
+      user.token = token;
+      const [response, data] = await user.fetchData();
+      if (response.status === 200) {
+        user.id = data.id;
+        user.username = data.username;
+        user.createdAt = new Date(data.createdAt);
+        user.updatedAt = new Date(data.updatedAt);
+        user.permissions = data.permissions;
+        user.preferredName = data.preferredName;
+        user.loggedIn = true;
+        setUser(user);
+        setUpdate(!update);
+      }
+    };
+    doStuff();
+  }, []);
   return (
     <BrowserRouter>
-      <Header></Header>
+      <Header user={user}></Header>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login user={user} setUser={setUser} />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={
+            <Login
+              user={user}
+              setUser={setUser}
+              setUpdate={setUpdate}
+              update={update}
+            />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Register
+              user={user}
+              setUser={setUser}
+              setUpdate={setUpdate}
+              update={update}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
