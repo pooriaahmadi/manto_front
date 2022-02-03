a4918d4f5acaabab276a488200779b212b3fec97
pooriaahmadi <pooriababak444@gmail.com>
2022-02-03 15:24:15 -0500

    yeah

--- src/Classes/User.ts
+++ src/Classes/User.ts
@@ -1,3 +1,4 @@
+import { create } from "domain";
 import { json } from "stream/consumers";
 import { UserInputs } from "../types/interfaces";
 
@@ -13,6 +14,7 @@ class User {
   preferredName?: string;
   createdAt?: Date;
   updatedAt?: Date;
+  loggedIn: boolean = false;
   constructor({
     endpoint,
     createdAt,
@@ -38,6 +40,33 @@ class User {
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
+  register = async ({
+    username,
+    password,
+    email,
+    preferredName,
+  }: {
+    username: string;
+    password: string;
+    email: string;
+    preferredName?: string;
+  }): Promise<[Response, any]> => {
+    const response = await fetch(`${this.endpoint}/users/`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        email,
+        preferredName,
+      }),
+      method: "post",
+    });
+    const data = await response.json();
+    return [response, data];
+  };
   login = async ({
     username,
     email,
@@ -46,7 +75,7 @@ class User {
     username?: string;
     email?: string;
     password: string;
-  }) => {
+  }): Promise<[Response, any]> => {
     const body: { [key: string]: any } = {
       password: password,
     };
@@ -65,6 +94,26 @@ class User {
     });
     const data = await response.json();
     return [response, data];
+  };
+  fetchData = async (): Promise<[Response, any]> => {
+    const response = await fetch(this.endpoint + "/users/" + this.token);
+    const data = await response.json();
+    return [response, data];
+  };
+  copy = (): User => {
+    return new User({
+      endpoint: this.endpoint,
+      createdAt: this.createdAt,
+      email: this.email,
+      id: this.id,
+      permissions: this.permissions,
+      preferredName: this.preferredName,
+      token: this.token,
+      updatedAt: this.updatedAt,
+      username: this.username,
+      verificationToken: this.verificationToken,
+      verified: this.verified,
+    });
   };
 }
 export default User;
